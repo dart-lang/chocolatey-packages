b94175d82ed17451b2ef44f94e702d0b6288d6d3
Alexander Thomas <athom@google.com>
2018-04-19 23:05:52 +0200

    Release dart-sdk-dev.2.0.0.48

--- Dart SDK - Dev Channel/chocolateyInstall.ps1
+++ Dart SDK - Dev Channel/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'dart-sdk-dev'
 
-$url = 'https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.47.0/sdk/dartsdk-windows-ia32-release.zip'
-$url64 = 'https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.47.0/sdk/dartsdk-windows-x64-release.zip'
+$url = 'https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.48.0/sdk/dartsdk-windows-ia32-release.zip'
+$url64 = 'https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.48.0/sdk/dartsdk-windows-x64-release.zip'
 
 $unzipLocation = Get-BinRoot
 $installDir = Join-Path $unzipLocation "dart-sdk-dev"
@@ -19,9 +19,9 @@ $packageArgs = @{
   url            = $url
   url64bit       = $url64
   unzipLocation  = $unzipLocation
-  checksum       = '9400d0f5db48316a3f6e87fb33c394a96c6a893e8189d838b2f74c4c1fe7e368'
+  checksum       = 'd3837220dc6e2e0aa2858240e7435d204eb56dd9b4345000ab552faa8e2aeb48'
   checksumType   = 'sha256'
-  checksum64     = '3bee80b5aaca8a52de2a5dfb5144bc32be20e2b58c40ac524610b9bdf95b51f3'
+  checksum64     = 'b3d2e3f2622ec14264412fb8ad2d30ca80e83a57c7a79db5e99548e357354232'
   checksumType64 = 'sha256'
 }
 
