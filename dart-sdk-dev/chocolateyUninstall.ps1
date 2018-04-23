12d67d3278a8af905bbe38d3c5c0dfd857f182cb
Alexander Thomas <athom@google.com>
2018-04-23 20:10:23 +0200

    Prepare to publish dev releases to dart-sdk package
    * Deprecate dart-sdk-dev package.
    * Add additional links to the nuspec.
    * Use replacement token for version in nuspec.
    * Use replacement tokens for channel, version, checksum and checksum64 in install script.
    * Delete nupkg files from repo.

--- dart-sdk-dev/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-$unzipLocation = Get-BinRoot
-$installDir = Join-Path $unzipLocation "dart-sdks"
-
-if (test-path $installDir) {
-	Remove-Item $installDir -Recurse -Force
-}
--- dart-sdk/chocolateyInstall.ps1
+++ dart-sdk/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'dart-sdk'
 
-$url = 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.2/sdk/dartsdk-windows-ia32-release.zip'
-$url64 = 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.2/sdk/dartsdk-windows-x64-release.zip'
+$url = 'https://storage.googleapis.com/dart-archive/channels/$channel$/release/$version$/sdk/dartsdk-windows-ia32-release.zip'
+$url64 = 'https://storage.googleapis.com/dart-archive/channels/$channel$/release/$version$/sdk/dartsdk-windows-x64-release.zip'
 
 $unzipLocation = Get-BinRoot
 $installDir = Join-Path $unzipLocation "dart-sdk"
@@ -19,9 +19,9 @@ $packageArgs = @{
   url            = $url
   url64bit       = $url64
   unzipLocation  = $unzipLocation
-  checksum       = '26b73442d8a910431ac9efdc38421ab6e743a556d830536f86c0a77902f17e5d'
+  checksum       = '$checksum$'
   checksumType   = 'sha256'
-  checksum64     = '513ce6d59caeed8444ccacf6021fa5e60e2b6c7a37dae80a693f6d90f14b5caa'
+  checksum64     = '$checksum64$'
   checksumType64 = 'sha256'
 }
 
